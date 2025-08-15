b4b6d625868f2cfa164bb3c0b7429a455a93df79
Deepak973 <deepak.rj.dr@gmail.com>
2025-08-15 17:53:18 +0530

    fix: app name

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,18 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-import { APP_BUTTON_TEXT, APP_DESCRIPTION, APP_ICON_URL, APP_NAME, APP_OG_IMAGE_URL, APP_PRIMARY_CATEGORY, APP_SPLASH_BACKGROUND_COLOR, APP_TAGS, APP_URL, APP_WEBHOOK_URL } from './constants';
-import { APP_SPLASH_URL } from './constants';
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import {
+  APP_BUTTON_TEXT,
+  APP_DESCRIPTION,
+  APP_ICON_URL,
+  APP_NAME,
+  APP_OG_IMAGE_URL,
+  APP_PRIMARY_CATEGORY,
+  APP_SPLASH_BACKGROUND_COLOR,
+  APP_TAGS,
+  APP_URL,
+  APP_WEBHOOK_URL,
+} from "./constants";
+import { APP_SPLASH_URL } from "./constants";
 
 interface FrameMetadata {
   version: string;
@@ -16,7 +27,7 @@ interface FrameMetadata {
   description?: string;
   primaryCategory?: string;
   tags?: string[];
-};
+}
 
 interface FrameManifest {
   accountAssociation?: {
@@ -54,26 +65,26 @@ export function getFrameEmbedMetadata(ogImageUrl?: string) {
 
 export async function getFarcasterMetadata(): Promise<FrameManifest> {
   if (!APP_URL) {
-    throw new Error('NEXT_PUBLIC_URL not configured');
+    throw new Error("NEXT_PUBLIC_URL not configured");
   }
 
   // Get the domain from the URL (without https:// prefix)
   const domain = new URL(APP_URL).hostname;
-  console.log('Using domain for manifest:', domain);
+  console.log("Using domain for manifest:", domain);
 
   return {
     accountAssociation: {
       header: process.env.ACCOUNT_ASSOCIATION_HEADER!,
       payload: process.env.ACCOUNT_ASSOCIATION_PAYLOAD!,
-      signature: process.env.ACCOUNT_ASSOCIATION_SIGNATURE!
+      signature: process.env.ACCOUNT_ASSOCIATION_SIGNATURE!,
     },
     frame: {
       version: "1",
-      name: APP_NAME ?? "Frames v2 Demo",
+      name: APP_NAME ?? "WhoCast",
       iconUrl: APP_ICON_URL,
       homeUrl: APP_URL,
       imageUrl: APP_OG_IMAGE_URL,
-      buttonTitle: APP_BUTTON_TEXT ?? "Launch Frame",
+      buttonTitle: APP_BUTTON_TEXT ?? "Launch WhoCast",
       splashImageUrl: APP_SPLASH_URL,
       splashBackgroundColor: APP_SPLASH_BACKGROUND_COLOR,
       webhookUrl: APP_WEBHOOK_URL,

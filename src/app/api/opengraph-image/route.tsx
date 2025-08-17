c040fb5ad2843e2778e8c3e005b015ed945ad01a
Deepak973 <deepak.rj.dr@gmail.com>
2025-08-17 17:23:43 +0530

    chore: udpate og image

--- src/app/api/opengraph-image/route.tsx
+++ src/app/api/opengraph-image/route.tsx
@@ -5,18 +5,12 @@ export const dynamic = "force-dynamic";
 export async function GET() {
   return new ImageResponse(
     (
-      <div tw="flex h-full w-full flex-col justify-center items-center relative bg-primary">
-        <div tw="flex w-96 h-96 rounded-full overflow-hidden mb-8 border-8 border-white">
-          <img
-            src={"https://whocast.vercel.app/og-image.png"}
-            alt="WhoCast Logo"
-            tw="w-full h-full object-cover"
-          />
-        </div>
-
-        <h1 tw="text-8xl text-purple-500">
-          Test your knowledge of your Farcaster friends
-        </h1>
+      <div tw="flex h-full w-full relative">
+        <img
+          src={"https://whocast.vercel.app/og-image.png"}
+          alt="WhoCast"
+          tw="w-full h-full object-cover"
+        />
       </div>
     ),
     {
